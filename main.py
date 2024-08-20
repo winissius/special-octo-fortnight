d55c0ac49d21d37ccbd56c0ebd49e0882a9bb9b8
W. Machado <104407345+winissius@users.noreply.github.com>
2024-08-20 05:43:42 -0300

    Outro teste

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ async def root():
 
 @app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num aleatorio": random.randint(0, 1000)}
+    return {"teste": True, "num aleatorio": random.randint(0, 2000)}
