103ed99b5160b1e305505c5351149f54ef1b660b
W. Machado <104407345+winissius@users.noreply.github.com>
2024-09-01 09:20:55 -0300

    Add Docker Integration CI

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 import random
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Estudante(BaseModel):
+    nome = str
+    curso = str
+    ativo = bool
 
 @app.get("/helloworld")
 async def root():
@@ -11,3 +17,18 @@ async def root():
 @app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": True, "num aleatorio": random.randint(0, 2000)}
+
+
+@app.post("/estudante/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+
+@app.put("/estudante/update/{id_estudante}") # put serve para atualizar algo ja cadastrado
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+
+@app.delete("/estudante/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
